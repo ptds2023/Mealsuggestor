5255263f76a64bcf06a7d8ade1ade334e126641d
Samsamou <samy.maurer@gmail.com>
2023-12-16 17:39:17 +0100

    Ideads for the functions to incorporate for the shiny app. Maybe there is more of them. We have to think about it

---
+++ R/filter_by_time.R
@@ -0,0 +1,1 @@
+filter_by_time(): Filters recipes within a specified preparation and cooking time range.
---
+++ R/get_recipes.R
@@ -0,0 +1,2 @@
+get_recipes()
+Retrieves recipes based on user inputs like ingredients, cuisine, and dietary preferences.
