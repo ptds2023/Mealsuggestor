088336124fd08423826d3f2540c47fdd992cc8ce
Samsamou <samy.maurer@gmail.com>
2023-12-18 16:43:32 +0100

    Removed the Prep and cook time as I already have it in my filter time function

--- R/get_recipes.R
+++ R/get_recipes.R
@@ -4,18 +4,16 @@
 #' cuisine, diet, maximum preparation time, and maximum cooking time.
 #'
 #' @param data Data frame containing the recipes.
-#' @param ingredients Vector of ingredients for filtering (optional).
-#' @param cuisine String specifying the cuisine type for filtering (optional).
-#' @param diet String specifying the diet type for filtering (optional).
-#' @param prep_time_max Numeric value for maximum preparation time in minutes (optional).
-#' @param cook_time_max Numeric value for maximum cooking time in minutes (optional).
+#' @param ingredients Vector of ingredients for filtering
+#' @param cuisine String specifying the cuisine type for filtering
+#' @param diet String specifying the diet type for filtering
 #' @return A data frame of filtered recipes.
 #' @examples
 #' get_recipes(data, ingredients = c("tomato", "chicken"),
 #'            cuisine = "Italian", diet = "Gluten Free",
 #'            prep_time_max = 30, cook_time_max = 60)
 #' @export
-get_recipes <- function(data, ingredients = NULL, cuisine = NULL, diet = NULL, prep_time_max = Inf, cook_time_max = Inf) {
+get_recipes <- function(data, ingredients = NULL, cuisine = NULL, diet = NULL) {
   # Filter by ingredients
   if (!is.null(ingredients)) {
     data <- data[grepl(paste(ingredients, collapse = "|"), data$ingredients), ]
@@ -29,16 +27,6 @@ get_recipes <- function(data, ingredients = NULL, cuisine = NULL, diet = NULL, p
   # Filter by dietd
   if (!is.null(diet)) {
     data <- data[data$diet == diet, ]
-  }
-
-  # Filter by prep time if provided
-  if (!is.null(prep_time_max)) {
-    data <- data[data$prep_time <= prep_time_max, ]
-  }
-
-  # Filter by cook time if provided
-  if (!is.null(cook_time_max)) {
-    data <- data[data$cook_time <= cook_time_max, ]
   }
 
   return(data)
