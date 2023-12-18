8e0c340246b02d1344d801e2e26dafaca2e1fdc8
Samsamou <samy.maurer@gmail.com>
2023-12-18 22:56:04 +0100

    tweeked the UI

--- R/app.R
+++ R/app.R
@@ -1,20 +1,51 @@
 library(shiny)
+library(shinythemes)
+library(htmltools)
 #library(Mealsuggestor)
 
-ui <- fluidPage(
-  titlePanel("Recipe Finder"),
-  sidebarLayout(
-    sidebarPanel(
-      selectInput("cuisine", "Choose Cuisine:", choices = c("Italian", "French", "American")),
-      selectInput("diet", "Choose Diet:", choices = c("Vegan", "Vegetarian", "None")), # Update choices
-      numericInput("n", "Number of Recipes:", 5),
-      actionButton("submit", "Search")
-    ),
-    mainPanel(
-      tableOutput("results")
+cuisine_vec <- unique(recipes$cuisine)
+diet_vec <- unique(recipes$diet)
+
+mealApp <- function(){
+  # Define UI
+  ui <- fluidPage(
+    theme = shinythemes::shinytheme("superhero"),
+    titlePanel("Meal Suggestor"),
+    navbarPage("Meals", id = "navbar",
+               tabPanel("Meal List",
+                        sidebarLayout(
+                          sidebarPanel(
+                            selectInput("cuisine", "Cuisine", choices = c("Choose" = "", cuisine_vec), selected = "Choose", multiple = FALSE),
+                            selectInput("diet", "Diet", choices = c("Choose" = "", diet_vec), selected = "Choose"),
+                            actionButton("clearButton", "Clear Selections"),
+                            actionButton("surpriseMeButton", "Surprise Me", icon = icon("random")),
+                            radioButtons("radio_time", "Prep/Cook Time",
+                                         c("Short" = "short",
+                                           "Medium" = "medium",
+                                           "Long" = "long"))
+                          ),
+                          mainPanel(
+                            uiOutput("mealList")
+                          )
+                        )
+               ),
+               tabPanel("Meal Details",
+                        mainPanel(
+                          uiOutput("mealDetails")
+                        )
+               ),
+               tabPanel("Information",
+                        mainPanel(
+                          h3("How to Use the Meal Suggestor"),
+                          p("Welcome to the Meal Suggestor! Here's how you can use this app:"),
+                          # Instructions and details
+                        )
+               )
     )
+    # Additional JavaScript if needed
   )
-)
+  # Server logic
+
 
 server <- function(input, output) {
   results <- eventReactive(input$submit, {
@@ -25,5 +56,7 @@ server <- function(input, output) {
     results()
   })
 }
+shinyApp(ui, server)
+}
 
-shinyApp(ui = ui, server = server)
+mealApp()
