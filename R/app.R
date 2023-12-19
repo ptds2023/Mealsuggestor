0a3efdb83e149cd2e6e508cd8ae93ca689d099d2
Samsamou <samy.maurer@gmail.com>
2023-12-19 14:46:29 +0100

    Shiny app with choose of - cuisine - Diet - Ingredient - Prep time - coock time - number of recipes - rating - clear selection - random generator

--- R/app.R
+++ R/app.R
@@ -1,7 +1,7 @@
 library(shiny)
 library(shinythemes)
 library(htmltools)
-#library(Mealsuggestor)
+library(Mealsuggestor)
 
 cuisine_vec <- unique(recipes$cuisine)
 diet_vec <- unique(recipes$diet)
@@ -9,7 +9,7 @@ diet_vec <- unique(recipes$diet)
 mealApp <- function(){
   # Define UI
   ui <- fluidPage(
-    theme = shinythemes::shinytheme("superhero"),
+    theme = shinythemes::shinytheme("slate"),
     titlePanel("Meal Suggestor"),
     navbarPage("Meals", id = "navbar",
                tabPanel("Meal List",
@@ -17,15 +17,23 @@ mealApp <- function(){
                           sidebarPanel(
                             selectInput("cuisine", "Cuisine", choices = c("Choose" = "", cuisine_vec), selected = "Choose", multiple = FALSE),
                             selectInput("diet", "Diet", choices = c("Choose" = "", diet_vec), selected = "Choose"),
+                            textInput("ingredient", "Enter an Ingredient", value = ""),
+                            sliderInput("prepTimeRange", "Preparation Time (minutes):",
+                                        min = 0, max = 120, value = c(10, 30), step = 5),
+                            sliderInput("cookTimeRange", "Cooking Time (minutes):",
+                                        min = 0, max = 120, value = c(15, 45), step = 5),
+                            sliderInput("numTopRecipes", "Number of top recipes to display:",
+                                        min = 1, max = 10, value = 5),
+                            selectInput("sortField", "Sort by:",
+                                        choices = list("Rating" = "rating",
+                                                       "Preparation Time" = "prep_time",
+                                                       "Cooking Time" = "cook_time"),
+                                        selected = "rating"),
                             actionButton("clearButton", "Clear Selections"),
-                            actionButton("surpriseMeButton", "Surprise Me", icon = icon("random")),
-                            radioButtons("radio_time", "Prep/Cook Time",
-                                         c("Short" = "short",
-                                           "Medium" = "medium",
-                                           "Long" = "long"))
-                          ),
+                            actionButton("randomRecipeButton", "I do not know what I want...", icon = icon("random"))),
                           mainPanel(
-                            uiOutput("mealList")
+                            uiOutput("mealList"),
+                            uiOutput("randomMealDetails")
                           )
                         )
                ),
@@ -38,23 +46,97 @@ mealApp <- function(){
                         mainPanel(
                           h3("How to Use the Meal Suggestor"),
                           p("Welcome to the Meal Suggestor! Here's how you can use this app:"),
-                          # Instructions and details
+                          tags$ul(
+                            tags$li("Select a cuisine from the 'Cuisine' dropdown."),
+                            tags$li("Choose a diet preference from the 'Diet' dropdown."),
+                            tags$li("Enter ingredients in the provided field."),
+                            tags$li("Adjust preparation and cooking time using the sliders."),
+                            tags$li("Click 'Surprise Me' for a random meal suggestion."),
+                            tags$li("Use 'Clear Selections' to reset all inputs.")
+                          ),
+                          p("Dont ever be bored when you want to cook :)")
                         )
                )
     )
-    # Additional JavaScript if needed
   )
-  # Server logic
 
 
-server <- function(input, output) {
-  results <- eventReactive(input$submit, {
-    get_recipes(cuisine = input$cuisine, diet = input$diet, n = input$n) #put here (data=recipes) to use it as an argument
+server <- function(input, output, session) {
+
+  # Show welcome message when the app starts
+  observe({
+    showModal(modalDialog(
+      title = "Welcome to Meal Suggestor!",
+      "Explore a variety of meals based on your preferences. Get started by selecting options on the left.",
+      easyClose = TRUE,
+      footer = NULL
+    ))
   })
 
-  output$results <- renderTable({
-    results()
+  # Reactive expression to filter and sort recipes based on user input
+  filteredRecipes <- reactive({
+    # Using get_recipes for basic filtering by cuisine, diet, etc.
+    basicFiltered <- get_recipes(data = recipes, cuisine = input$cuisine, diet = input$diet)
+
+    # Further filtering by preparation and cooking time
+    timeFiltered <- filter_by_time(data = basicFiltered,
+                                   min_prep_time = input$prepTimeRange[1],
+                                   max_prep_time = input$prepTimeRange[2],
+                                   min_cook_time = input$cookTimeRange[1],
+                                   max_cook_time = input$cookTimeRange[2])
+
+    # Sorting the filtered data
+    sortedRecipes <- sort_recipes(data = timeFiltered, sort_by = input$sortField, ascending = FALSE)
+
+    # Getting top n recipes
+    get_top_n_recipes(data = sortedRecipes, n = input$numTopRecipes)
   })
+
+  # Dynamic UI for displaying filtered recipes
+  output$mealList <- renderUI({
+    meals <- filteredRecipes()
+
+    # Check if any meals are found
+    if (nrow(meals) > 0) {
+      # Create UI elements for displaying meals
+      # For example, using a simple list of meal titles
+      lapply(meals$recipe_title, function(title) {
+        div(class = "meal-item", title)
+      })
+    } else {
+      "No meals found based on your criteria."
+    }
+  })
+
+  # Handle the random recipe button click
+  observeEvent(input$randomRecipeButton, {
+    randomMeal <- random_recipe(data = recipes)
+    output$randomMealDetails <- renderUI({
+      if (!is.null(randomMeal)) {
+        # Create UI elements to display the details
+        h3(randomMeal$recipe_title)
+        # Add more details as needed
+      } else {
+        "No random meal found."
+      }
+    })
+  })
+
+  observeEvent(input$clearButton, {
+    # Reset the inputs to their default values
+    updateSelectInput(session, "cuisine", selected = "Choose")
+    updateSelectInput(session, "diet", selected = "Choose")
+    updateTextInput(session, "ingredient", value = "")
+    updateSliderInput(session, "prepTimeRange", value = c(10, 30))
+    updateSliderInput(session, "cookTimeRange", value = c(15, 45))
+    updateSliderInput(session, "numTopRecipes", value = 5)
+    updateSelectInput(session, "sortField", selected = "rating")
+    output$randomMealDetails <- renderUI({})
+  })
+
+
+
+
 }
 shinyApp(ui, server)
 }
