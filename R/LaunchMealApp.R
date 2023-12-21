4b0fcabcd7c4a89ab7689bc206b8f22d62ec9985
Samsamou <131663252+Samsamou@users.noreply.github.com>
2023-12-21 01:29:48 +0100

    Remove useless comments

--- R/LaunchMealApp.R
+++ R/LaunchMealApp.R
@@ -1,8 +1,6 @@
 #' Launch Meal Suggestor Shiny App
 #'
 #' This function starts the Meal Suggestor Shiny application contained within the package.
-#' It assumes that the Shiny app's UI and server logic are defined in an external script named 'app.R'.
-#'
 #' @importFrom shiny runApp
 #' @export
 #'
